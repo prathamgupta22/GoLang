53125c02a6a0c9de48a9ee880f0e3f7fc8e74a0c
prathamgupta22 <pratham.guptapg01@gmail.com>
2024-04-11 17:31:58 +0530

    directory

---
+++ Basics/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"mylearning/myutil"
+)
+
+func main() {
+	fmt.Println("Learn Go language by Hello World")
+	fmt.Println("----------------------------------")
+
+	myutil.PrintMessage("Hello World")
+}
